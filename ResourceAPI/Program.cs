cfb55e2b368d940d23876f2f47bcea7827cd8f51
Gokul Kannan <gokulrgk1110@gmail.com>
2025-01-04 15:11:35 +0530

    Add project files.

---
+++ ResourceAPI/Program.cs
@@ -0,0 +1,13 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();

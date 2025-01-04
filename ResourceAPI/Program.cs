e560e1689818a108f59a3003b888926f981a70e4
Gokul Kannan <gokulrgk1110@gmail.com>
2025-01-04 18:12:07 +0530

    Added Data using Raw SQL in Migration file and registered the service

--- ResourceAPI/Program.cs
+++ ResourceAPI/Program.cs
@@ -1,5 +1,7 @@
 using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using ResourceAPI;
+using ResourceAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddScoped<ICoffeeShopService, CoffeeShopService>();
 
 var app = builder.Build();
 
